e9b6a0bc8f5b2bc731965b156ab4eff574d22d2f
EceNarin <la.crymosa321@gmail.com>
2022-07-17 01:02:10 +0300

    add

---
+++ src/test/java/restfulTest/C05_PUTRequest.java
@@ -0,0 +1,44 @@
+package restfulTest;
+
+import org.junit.Test;
+
+public class C05_PUTRequest {
+    @Test
+    public void put() {
+        /*
+        curl -X PUT \
+  https://restful-booker.herokuapp.com/booking/1 \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -H 'Cookie: token=abc123' \
+  -d '{
+    "firstname" : "James",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}'
+         */
+        // 1 -Request url ve body'sini hazirlamak
+        // 2- Expected Data'yi hazirla
+        /*
+        {
+    "firstname" : "James",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}
+         */
+        // 3- Response'u kaydet
+        // 4- Assertion'lari yap
+    }
+}
