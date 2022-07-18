1d0dff2d4e25aa0f7de2f17a183b4efef40a714c
EceNarin <la.crymosa321@gmail.com>
2022-07-18 13:19:56 +0300

    add

---
+++ src/test/java/restfulTest/C07_Delete.java
@@ -0,0 +1,54 @@
+package restfulTest;
+
+import baseUrls.BaseUrlRestful;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+import utilities.ConfigReader;
+
+import static io.restassured.RestAssured.given;
+
+public class C07_Delete extends BaseUrlRestful {
+    @Test
+    public void cookie() {
+        /*
+curl -X DELETE \
+  https://restful-booker.herokuapp.com/booking/1 \
+  -H 'Content-Type: application/json' \
+  -H 'Cookie: token=abc123'
+         */
+        // 1 -Request url ve body'sini hazirlamak
+        specRestful.pathParams("pp1","booking","pp2",3956);
+        RequestSpecification req= given()
+                .spec(specRestful)
+                .contentType(ContentType.JSON)
+                .cookie("token", ConfigReader.getProperty("token"))
+                .when();
+        Response response=req.delete("/{pp1}/{pp2}");
+        response.prettyPrint();
+        // 2- Expected Data'yi hazirla
+        // 3- Response'u kaydet
+        // 4- Assertion'lari yap
+        response.then()
+                .assertThat()
+                .statusCode(201);
+
+
+    }
+
+    @Test
+    public void authorisation() {
+        /*
+        curl -X DELETE \
+  https://restful-booker.herokuapp.com/booking/1 \
+  -H 'Content-Type: application/json' \
+  -H 'Authorisation: Basic YWRtaW46cGFzc3dvcmQxMjM=
+         */
+        // 1 -Request url ve body'sini hazirlamak
+        // 2- Expected Data'yi hazirla
+        // 3- Response'u kaydet
+        // 4- Assertion'lari yap
+    }
+
+}
