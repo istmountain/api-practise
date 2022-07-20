df4b7ffea9301d349464d745348fa333bb84894e
EceNarin <la.crymosa321@gmail.com>
2022-07-20 23:22:58 +0300

    add

---
+++ src/test/java/petSwaggerTests/C06_PostPetId.java
@@ -0,0 +1,59 @@
+package petSwaggerTests;
+
+import baseUrls.BaseUrlPet;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C06_PostPetId extends BaseUrlPet {
+    @Test
+    public void name() {
+        /*
+        {
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "doggie",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+}
+curl -X 'POST' \
+  'https://petstore.swagger.io/v2/pet/9' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'name=cats&status=avaliable'
+         */
+        specPet.pathParams("pp1","pet","pp2",9);
+        //create request body
+        JSONObject body=new JSONObject();
+        body.put("name","cats");
+        body.put("status","avaliable");
+        Response response=given()
+                .spec(specPet)
+                //.contentType("application/x-www-form-urlencoded")
+                .accept(ContentType.JSON)
+                .body(body)
+                .post("/{pp1}/{pp2}");
+        response.prettyPrint();
+        System.out.println(response.getContentType());
+        response
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+
+    }
+}
