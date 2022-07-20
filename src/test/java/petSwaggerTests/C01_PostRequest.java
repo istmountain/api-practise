be919b5f44d6fe8f674ef9a3c0c02b3b1bb79c2b
EceNarin <la.crymosa321@gmail.com>
2022-07-20 21:53:08 +0300

    add

--- src/test/java/petSwaggerTests/C01_PostRequest.java
+++ src/test/java/petSwaggerTests/C01_PostRequest.java
@@ -1,8 +1,17 @@
 package petSwaggerTests;
 
+import baseUrls.BaseUrlPet;
+import com.google.gson.JsonArray;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
 
-public class C01_PostRequest {
+import static io.restassured.RestAssured.given;
+
+public class C01_PostRequest extends BaseUrlPet {
 
 
     @Test
@@ -32,6 +41,39 @@ public class C01_PostRequest {
   "status": "available"
 }'
          */
-
+        // 1 -Request url ve body'sini hazirlamak
+        JSONObject requestBody=new JSONObject();
+        JSONObject category=new JSONObject();
+        JSONObject tagsBody=new JSONObject();
+        JSONArray photo=new JSONArray();
+        JSONArray tags=new JSONArray();
+        tagsBody.put("id", 0);
+        tagsBody.put("name", "string");
+        tags.put(tagsBody);
+        category.put("id", 0);
+        category.put("name","string");
+        photo.put("Strings");
+        requestBody.put("id", 0);
+        requestBody.put("category",category);
+        requestBody.put("name", "doggie");
+        requestBody.put("photoUrls",photo);
+        requestBody.put("tags",tags);
+        requestBody.put("status", "available");
+        specPet.pathParam("pp1","pet");
+        Response response=given()
+                .spec(specPet)
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .when()
+                .post("/{pp1}");
+        response.prettyPrint();
+        // 2- Expected Data'yi hazirla
+        // 3- Response'u kaydet
+        JsonPath actual=response.jsonPath();
+        System.out.println(actual.prettyPrint());
+        // 4- Assertion'lari yap
+        response.then()
+                .assertThat()
+                .statusCode(200);
     }
 }
---
+++ src/test/java/petSwaggerTests/C02_GetFindByStatus.java
@@ -0,0 +1,9 @@
+package petSwaggerTests;
+
+public class C02_GetFindByStatus {
+    /*
+    curl -X 'GET' \
+  'https://petstore.swagger.io/v2/pet/findByStatus?status=available' \
+  -H 'accept: application/json'
+     */
+}
