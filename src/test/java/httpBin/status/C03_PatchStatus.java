8399ec9c1490e59b0a1c6fba80ad2fc6ec35ebcb
EceNarin <la.crymosa321@gmail.com>
2022-07-24 17:26:01 +0300

    add

--- src/test/java/httpBin/status/C03_PatchStatus.java
+++ src/test/java/httpBin/status/C03_PatchStatus.java
@@ -10,9 +10,12 @@ import java.net.URL;
 
 import static io.restassured.RestAssured.given;
 
-public class C02_GetStatus {
-    /*
-    Code	Description
+public class C03_PatchStatus {
+    @Test
+    public void statusPatch300() {
+        /*
+
+Code	Description
 100
 Informational responses
 200
@@ -23,23 +26,20 @@ Redirection
 Client Errors
 500
 Server Errors
-     */
-
-    @Test
-    public void status300() {
+         */
         //300 (MULTIPLE CHOICES)
         Response response=given()
                 .accept(ContentType.JSON)
                 .when()
-                .get("http://httpbin.org/status/300");
+                .patch("http://httpbin.org/status/300");
         /*
-        Date: Sun, 24 Jul 2022 13:23:47 GMT
-Content-Type: text/html; charset=utf-8
-Content-Length: 0
-Connection: keep-alive
-Server: gunicorn/19.9.0
-Access-Control-Allow-Origin: *
-Access-Control-Allow-Credentials: true
+ access-control-allow-credentials: true
+ access-control-allow-origin: *
+ connection: keep-alive
+ content-length: 0
+ content-type: text/html; charset=utf-8
+ date: Sun, 24 Jul 2022 14:06:50 GMT
+ server: gunicorn/19.9.0
          */
         response
                 .then()
@@ -53,23 +53,27 @@ Access-Control-Allow-Credentials: true
                 .header("Access-Control-Allow-Credentials", "true");
     }
     @Test
-    public void status300HTTP() throws IOException {
-        //300 (MULTIPLE CHOICES)
+    public void statusPatch300HTTP() throws IOException {
         URL url = new URL("http://httpbin.org/status/300");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("PATCH");
+        http.setDoOutput(true);
         http.setRequestProperty("Authorization", "Basic ZWNlOjEyMzQ1Njc=");
+        http.setRequestProperty("Content-Type", "");
+        http.setRequestProperty("Content-Length", "0");
 
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
+
     }
 
     @Test
-    public void status200() {
+    public void statusPatch200() {
         Response
                 response=given()
                 .contentType(ContentType.JSON)
                 .when()
-                .get("http://httpbin.org/status/200");
+                .patch("http://httpbin.org/status/200");
         /*
 200 (OK)
         Date: Sun, 24 Jul 2022 13:27:11 GMT
@@ -92,10 +96,14 @@ Access-Control-Allow-Credentials: true
                 .header("Access-Control-Allow-Credentials", "true");
     }
     @Test
-    public void status200HTTP() throws IOException {
+    public void statusPatch200HTTP() throws IOException {
         URL url = new URL("http://httpbin.org/status/200");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("PATCH");
+        http.setDoOutput(true);
         http.setRequestProperty("Authorization", "Basic ZWNlOjEyMzQ1Njc=");
+        http.setRequestProperty("Content-Type", "");
+        http.setRequestProperty("Content-Length", "0");
 
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
@@ -104,13 +112,13 @@ Access-Control-Allow-Credentials: true
 
 
     @Test
-    public void status400() {
+    public void statusPatch400() {
         Response
                 response=given()
                 .header("Authorization", "Basic ZWNlOjEyMzQ1Njc=")
                 .accept(ContentType.JSON)
                 .when()
-                .get("http://httpbin.org/status/400");
+                .patch("http://httpbin.org/status/400");
         /* 400 (BAD REQUEST)
         Date: Sun, 24 Jul 2022 13:29:36 GMT
 Content-Type: text/html; charset=utf-8
@@ -132,22 +140,35 @@ Access-Control-Allow-Credentials: true
                 .header("Access-Control-Allow-Credentials", "true");
     }
     @Test
-    public void status400HTTP() throws IOException {
+    public void statusPatch400HTTP() throws IOException {
         URL url = new URL("http://httpbin.org/status/400");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("PATCH");
+        http.setDoOutput(true);
         http.setRequestProperty("Authorization", "Basic ZWNlOjEyMzQ1Njc=");
+        http.setRequestProperty("Content-Type", "");
+        http.setRequestProperty("Content-Length", "0");
 
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
     }
 
     @Test
-    public void status500() {
+    public void statusPatch500() {
         Response
                 response=given()
                 .accept(ContentType.JSON)
                 .when()
-                .get("http://httpbin.org/status/500");
+                .patch("http://httpbin.org/status/500");
+              /* HTTP/1.1 500 INTERNAL SERVER ERROR
+Date: Sun, 24 Jul 2022 14:23:17 GMT
+Content-Type: text/html; charset=utf-8
+Content-Length: 0
+Connection: keep-alive
+Server: gunicorn/19.9.0
+Access-Control-Allow-Origin: *
+Access-Control-Allow-Credentials: true
+         */
         response
                 .then()
                 .assertThat()
@@ -160,7 +181,7 @@ Access-Control-Allow-Credentials: true
                 .header("Access-Control-Allow-Credentials", "true");
     }
     @Test
-    public void status500HTTP() throws IOException {
+    public void statusPatch500HTTP() throws IOException {
         /*  500 (INTERNAL SERVER ERROR)
         Date: Sun, 24 Jul 2022 13:30:43 GMT
 Content-Type: text/html; charset=utf-8
@@ -172,7 +193,11 @@ Access-Control-Allow-Credentials: true
          */
         URL url = new URL("http://httpbin.org/status/500");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("PATCH");
+        http.setDoOutput(true);
         http.setRequestProperty("Authorization", "Basic ZWNlOjEyMzQ1Njc=");
+        http.setRequestProperty("Content-Type", "");
+        http.setRequestProperty("Content-Length", "0");
 
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
