d889e538b552204a5f6fd1071f95eb82949b0c1c
EceNarin <la.crymosa321@gmail.com>
2022-07-24 15:51:38 +0300

    add

---
+++ src/test/java/httpBin/status/DeleteStatus.java
@@ -0,0 +1,15 @@
+package httpBin.status;
+
+import org.junit.Test;
+
+public class DeleteStatus {
+    @Test
+    public void reqDelete() {
+    }
+    @Test
+    public void resDelete() {
+    }
+    @Test
+    public void httpDelete() {
+    }
+}
