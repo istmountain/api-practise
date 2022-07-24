b7a184a3d99da9a3bf450c080ae25ed73218f8f6
EceNarin <la.crymosa321@gmail.com>
2022-07-24 15:56:11 +0300

    add

--- src/test/java/httpBin/status/DeleteStatus.java
+++ src/test/java/httpBin/status/DeleteStatus.java
@@ -2,6 +2,10 @@ package httpBin.status;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class DeleteStatus {
     @Test
     public void reqDelete() {
@@ -10,6 +14,13 @@ public class DeleteStatus {
     public void resDelete() {
     }
     @Test
-    public void httpDelete() {
+    public void httpDelete() throws IOException {
+        URL url = new URL("http://httpbin.org/status/200");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("DELETE");
+        http.setRequestProperty("Authorization", "Basic ZWNlOjEyMzQ1Njc=");
+
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        http.disconnect();
     }
 }
