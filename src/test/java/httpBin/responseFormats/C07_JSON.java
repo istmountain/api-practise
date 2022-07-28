8be87c0211b0083a1e4862813bcdad1deaad0005
EceNarin <la.crymosa321@gmail.com>
2022-07-28 13:33:23 +0300

    add

--- src/test/java/httpBin/responseFormats/C07_JSON.java
+++ src/test/java/httpBin/responseFormats/C07_JSON.java
@@ -66,10 +66,9 @@ Response headers
         JSONObject ininner=new JSONObject();
         JSONArray slides=new JSONArray();
         JSONArray items=new JSONArray();
-        HashMap<String,String> map=new HashMap<>();
-        map.put(     "Why <em>WonderWidgets</em> are great",
-                "Who <em>buys</em> WonderWidgets");
-        items.put(0,map);
+        JSONArray map=new JSONArray();
+        items.put(0,"Why <em>WonderWidgets</em> are great");
+        items.put(1,"Who <em>buys</em> WonderWidgets");
         ininner.put("items",items);
         ininner.put("title", "Overview");
         ininner.put("type", "all");
@@ -119,10 +118,88 @@ Response headers
         assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(0).get("title"),((Map)act.getList("slideshow.slides").get(0)).get("title")) ;
         assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(0).get("type"),((Map)act.getList("slideshow.slides").get(0)).get("type")) ;
         assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).getJSONArray("items").get(0),
-                (   ((ArrayList)  ((Map)act.getList("slideshow.slides").get(1)).get("items")).get(0) )  ) ;
+                (  ((ArrayList) ((Map)act.getList("slideshow.slides").get(1)).get("items"))).get(0)  ) ;
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).getJSONArray("items").get(1),
+                (  ((ArrayList) ((Map)act.getList("slideshow.slides").get(1)).get("items"))).get(1)  ) ;
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).get("title"),((Map)act.getList("slideshow.slides").get(1)).get("title")) ;
+
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).get("type"),((Map)act.getList("slideshow.slides").get(1)).get("type")) ;
+
 
     }
     @Test
     public void res() {
+        Response response=given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("http://httpbin.org/json");
+        response.prettyPrint();
+        //exp data
+        JSONObject exp=new JSONObject();
+        JSONObject slideShow=new JSONObject();
+        JSONObject inner=new JSONObject();
+        JSONObject ininner=new JSONObject();
+        JSONArray slides=new JSONArray();
+        JSONArray items=new JSONArray();
+        JSONArray map=new JSONArray();
+        items.put(0,"Why <em>WonderWidgets</em> are great");
+        items.put(1,"Who <em>buys</em> WonderWidgets");
+        ininner.put("items",items);
+        ininner.put("title", "Overview");
+        ininner.put("type", "all");
+        inner.put("title", "Wake up to WonderWidgets!");
+        inner.put("type", "all");
+        slides.put(0,inner);
+        slides.put(1,ininner);
+        slideShow.put("author", "Yours Truly");
+        slideShow.put("date","date of publication");
+        slideShow.put("slides",slides);
+        slideShow.put("title", "Sample Slide Show");
+        exp.put("slideshow",slideShow);
+        /*
+        {
+    "slideshow": {
+        "author": "Yours Truly",
+        "date": "date of publication",
+        "slides": [
+            {
+                "title": "Wake up to WonderWidgets!",
+                "type": "all"
+            },
+            {
+                "items": [
+                    "Why <em>WonderWidgets</em> are great",
+                    "Who <em>buys</em> WonderWidgets"
+                ],
+                "title": "Overview",
+                "type": "all"
+            }
+        ],
+        "title": "Sample Slide Show"
+    }
+}
+         */
+        System.out.println(exp);
+        //
+        JsonPath act=response.jsonPath();
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200);
+        assertEquals(exp.getJSONObject("slideshow").get("author"),act.get("slideshow.author"));
+        assertEquals(exp.getJSONObject("slideshow").get("date"),act.get("slideshow.date"));
+        assertEquals(exp.getJSONObject("slideshow").get("title"),act.get("slideshow.title"));
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(0).get("title"),((Map)act.getList("slideshow.slides").get(0)).get("title")) ;
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(0).get("type"),((Map)act.getList("slideshow.slides").get(0)).get("type")) ;
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).getJSONArray("items").get(0),
+                (  ((ArrayList) ((Map)act.getList("slideshow.slides").get(1)).get("items"))).get(0)  ) ;
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).getJSONArray("items").get(1),
+                (  ((ArrayList) ((Map)act.getList("slideshow.slides").get(1)).get("items"))).get(1)  ) ;
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).get("title"),((Map)act.getList("slideshow.slides").get(1)).get("title")) ;
+
+        assertEquals(exp.getJSONObject("slideshow").getJSONArray("slides").getJSONObject(1).get("type"),((Map)act.getList("slideshow.slides").get(1)).get("type")) ;
+
+
     }
 }
