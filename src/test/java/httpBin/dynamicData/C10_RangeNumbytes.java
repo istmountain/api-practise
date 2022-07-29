6c45456c818779c9d73381f2954d44fe0c422c83
EceNarin <la.crymosa321@gmail.com>
2022-07-29 16:45:35 +0300

    add

--- src/test/java/httpBin/dynamicData/C10_RangeNumbytes.java
+++ src/test/java/httpBin/dynamicData/C10_RangeNumbytes.java
@@ -12,25 +12,21 @@ import java.net.URL;
 
 import static io.restassured.RestAssured.given;
 
-public class C09_Links__GenerateAPageContainignNLinkstoOtherPagesWhichTodoSAme extends BaseHttpBin {
+public class C10_RangeNumbytes extends BaseHttpBin {
     /*
-    Curl
-curl -X GET "http://httpbin.org/links/4/5" -H "accept: text/html"
-Request URL
-http://httpbin.org/links/4/5
-Server response
-Code	Details
-200
+
 Response body
-Download
-<html><head><title>Links</title></head><body><a href='/links/4/0'>0</a> <a href='/links/4/1'>1</a> <a href='/links/4/2'>2</a> <a href='/links/4/3'>3</a> </body></html>
+Download file
 Response headers
+ accept-ranges: bytes
  access-control-allow-credentials: true
  access-control-allow-origin: *
  connection: keep-alive
- content-length: 167
- content-type: text/html; charset=utf-8
- date: Fri, 29 Jul 2022 13:01:12 GMT
+ content-length: 5
+ content-range: bytes 0-4/5
+ content-type: application/octet-stream
+ date: Fri, 29 Jul 2022 13:11:57 GMT
+ etag: range5
  server: gunicorn/19.9.0
 Responses
 Code	Description
@@ -39,7 +35,7 @@ Code	Description
 
     @Test
     public void http() throws IOException {
-        URL url = new URL("http://httpbin.org/links/4/5");
+        URL url = new URL("http://httpbin.org/range/5");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
@@ -47,13 +43,16 @@ Code	Description
     @Test
     public void req() {
         /*
-            Curl
-curl -X GET "http://httpbin.org/links/4/5" -H "accept: text/html"
-Request URL
-http://httpbin.org/links/4/5
+                Curl
+    curl -X GET "http://httpbin.org/range/5" -H "accept: application/octet-stream"
+    Request URL
+    http://httpbin.org/range/5
+    Server response
+    Code	Details
+    200
          */
-        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/links/4/5")
-                .setAccept("text/html").build();
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/range/5")
+                .setAccept("application/octet-stream").build();
         Response response=given()
                 .spec(req)
                 .when()
@@ -62,26 +61,30 @@ http://httpbin.org/links/4/5
         response.then()
                 .assertThat()
                 .statusCode(200)
-                .contentType("text/html");
+                .contentType("application/octet-stream");
     }
     @Test
     public void res() {
-                /*
+        /*
             Curl
-curl -X GET "http://httpbin.org/links/4/5" -H "accept: text/html"
-Request URL
-http://httpbin.org/links/4/5
+    curl -X GET "http://httpbin.org/range/5" -H "accept: application/octet-stream"
+    Request URL
+    http://httpbin.org/range/5
+    Server response
+    Code	Details
+    200
          */
-        specHttpbin.pathParams("pp1","links","pp2",4,"pp3",5);
+        specHttpbin.pathParams("pp1","range","pp2",5);
         Response response=given()
                 .spec(specHttpbin)
-                .accept("text/html")
+                .accept("application/octet-stream")
                 .when()
-                .get("/{pp1}/{pp2}/{pp3}");
+                .get("/{pp1}/{pp2}");
         response.prettyPrint();
         response.then()
                 .assertThat()
                 .statusCode(200)
-                .contentType("text/html");
+                .contentType("application/octet-stream");
+
     }
 }
