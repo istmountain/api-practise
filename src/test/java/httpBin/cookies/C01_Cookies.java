cc1ad6213f053e3eef6ddff8c99783b6b6f098ac
EceNarin <la.crymosa321@gmail.com>
2022-07-30 16:26:42 +0300

    add

--- src/test/java/httpBin/cookies/C01_Cookies.java
+++ src/test/java/httpBin/cookies/C01_Cookies.java
@@ -1,11 +1,16 @@
 package httpBin.cookies;
 
 import baseUrls.BaseHttpBin;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static io.restassured.RestAssured.given;
 
 public class C01_Cookies extends BaseHttpBin {
     @Test
@@ -26,9 +31,48 @@ Code	Details
     }
     @Test
     public void req() {
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/cookies").setContentType("application/json").build();
+        Response response=given()
+                .spec(req)
+                .when()
+                .get();
+        response.prettyPrint();
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json");
+                /*
+            Curl
+curl -X GET c -H "accept: application/json"
+Request URL
+http://httpbin.org/cookies
+Server response
+Code	Details
+200
+         */
     }
     @Test
     public void res() {
+        specHttpbin.pathParam("pp1","cookies");
+        Response response=given()
+                .spec(specHttpbin)
+                .accept("application/json")
+                .when()
+                .get("/{pp1}");
+        response.prettyPrint();
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json");
+                /*
+            Curl
+curl -X GET "http://httpbin.org/cookies" -H "accept: application/json"
+Request URL
+http://httpbin.org/cookies
+Server response
+Code	Details
+200
+         */
     }
     /*
     Curl
