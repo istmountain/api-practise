150ffee94d32b0dad5004bdc41562250547b944d
EceNarin <la.crymosa321@gmail.com>
2022-07-31 15:16:51 +0300

    add

--- src/test/java/httpBin/images/C05_Image_WEBP.java
+++ src/test/java/httpBin/images/C05_Image_WEBP.java
@@ -1,5 +1,6 @@
 package httpBin.images;
 
+import baseUrls.BaseHttpBin;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -11,30 +12,30 @@ import java.net.URL;
 
 import static io.restassured.RestAssured.given;
 
-public class C05_Image_WEBP {
+public class C05_Image_WEBP extends BaseHttpBin {
     @Test
     public void http() throws IOException {
-        URL url = new URL("http://httpbin.org/image/jpeg");
+        URL url = new URL("http://httpbin.org/image/webp");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
 
-        /*
-            Curl
-curl -X GET "http://httpbin.org/image/jpeg" -H "accept: image/jpeg"
+        /*    Curl
+curl -X GET "http://httpbin.org/image/webp" -H "accept: image/webp"
 Request URL
-http://httpbin.org/image/jpeg
+http://httpbin.org/image/webp
 Server response
 Code	Details
 200
+Response body
          */
     }
     @Test
     public void req() {
-        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/image/jpeg").build();
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/image/webp").build();
         Response response=given()
                 .spec(req)
-                .accept("image/jpeg")
+                .accept("image/webp")
                 .when()
                 .get();
         response.prettyPrint();
@@ -42,23 +43,24 @@ Code	Details
         response.then()
                 .assertThat()
                 .statusCode(200)
-                .contentType("image/jpeg");
+                .contentType("image/webp");
         /*
-            Curl
-curl -X GET "http://httpbin.org/image/jpeg" -H "accept: image/jpeg"
+    Curl
+curl -X GET "http://httpbin.org/image/webp" -H "accept: image/webp"
 Request URL
-http://httpbin.org/image/jpeg
+http://httpbin.org/image/webp
 Server response
 Code	Details
 200
+Response body
          */
     }
     @Test
     public void res() {
-        specHttpbin.pathParams("pp1","image","pp2","jpeg");
+        specHttpbin.pathParams("pp1","image","pp2","webp");
         Response response=given()
                 .spec(specHttpbin)
-                .accept("image/jpeg")
+                .accept("image/webp")
                 .when()
                 .get("/{pp1}/{pp2}");
         response.prettyPrint();
@@ -66,15 +68,16 @@ Code	Details
         response.then()
                 .assertThat()
                 .statusCode(200)
-                .contentType("image/jpeg");
+                .contentType("image/webp");
         /*
-            Curl
-curl -X GET "http://httpbin.org/image/jpeg" -H "accept: image/jpeg"
+    Curl
+curl -X GET "http://httpbin.org/image/webp" -H "accept: image/webp"
 Request URL
-http://httpbin.org/image/jpeg
+http://httpbin.org/image/webp
 Server response
 Code	Details
 200
+Response body
          */
     }
     /*
