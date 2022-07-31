5dfbe09c2334294498daa94c227776f94b8f9b07
EceNarin <la.crymosa321@gmail.com>
2022-08-01 00:51:21 +0300

    add

--- src/test/java/baseUrls/BaseReqresIn.java
+++ src/test/java/baseUrls/BaseReqresIn.java
@@ -5,7 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class BaseReqresIn {
-    RequestSpecification specReqres;
+   protected RequestSpecification specReqres;
     @Before
     public void setUo(){
         specReqres=new RequestSpecBuilder().setBaseUri("https://reqres.in/").build();
--- src/test/java/reqresIn/C01_GetListUsers.java
+++ src/test/java/reqresIn/C01_GetListUsers.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Map;
 
 import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
@@ -55,6 +56,7 @@ public class C01_GetListUsers extends BaseReqresIn {
         exp.put("total", 12);
         exp.put("total_pages", 2);
         exp.put("data",data);
+        System.out.println(exp);
         /*
         {
     "page": 1,
@@ -77,10 +79,64 @@ public class C01_GetListUsers extends BaseReqresIn {
         assertEquals(exp.get("per_page"),act.get("per_page"));
         assertEquals(exp.get("total"),act.get("total"));
         assertEquals(exp.get("total_pages"),act.get("total_pages"));
-        assertEquals( ( (JSONArray)exp.getJSONArray("data").get(0)).get(0), ( ( (JSONObject)((JSONArray)act.get("data")).get(0)).get("id") ) ) ;
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("id"),( ( (Map)act.getList("data").get(0)).get("id")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("email"),( ( (Map)act.getList("data").get(0)).get("email")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("first_name"),( ( (Map)act.getList("data").get(0)).get("first_name")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("last_name"),( ( (Map)act.getList("data").get(0)).get("last_name")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("avatar"),( ( (Map)act.getList("data").get(0)).get("avatar")) );
+
     }
     @Test
-    public void res() {
+    public void res() { //https://reqres.in/api/users?page=2
+        specReqres.pathParams("pp1","api","pp2","users").queryParam("page",2);
+        Response response=given()
+                .spec(specReqres)
+                .when()
+                .get("/{pp1}/{pp2}");
+        response.prettyPrint();
+        //expData
+        JSONObject exp=new JSONObject();
+        JSONArray data=new JSONArray();
+        JSONObject inner =new JSONObject();
+        inner.put("id", 7);
+        inner.put("email","michael.lawson@reqres.in");
+        inner.put("first_name", "Michael");
+        inner.put("last_name", "Lawson");
+        inner.put("avatar", "https://reqres.in/img/faces/7-image.jpg");
+        data.put(0,inner);
+        exp.put("page", 2);
+        exp.put("per_page", 6);
+        exp.put("total", 12);
+        exp.put("total_pages", 2);
+        exp.put("data",data);
+        System.out.println(exp);
+        /*
+        {
+    "page": 1,
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2,
+    "data": [
+        {
+            "id": 1,
+            "email": "george.bluth@reqres.in",
+            "first_name": "George",
+            "last_name": "Bluth",
+            "avatar": "https://reqres.in/img/faces/1-image.jpg"
+        },
+         */
+        //save response
+        JsonPath act=response.jsonPath();
+        //assert
+        assertEquals(exp.get("page"),act.get("page"));
+        assertEquals(exp.get("per_page"),act.get("per_page"));
+        assertEquals(exp.get("total"),act.get("total"));
+        assertEquals(exp.get("total_pages"),act.get("total_pages"));
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("id"),( ( (Map)act.getList("data").get(0)).get("id")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("email"),( ( (Map)act.getList("data").get(0)).get("email")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("first_name"),( ( (Map)act.getList("data").get(0)).get("first_name")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("last_name"),( ( (Map)act.getList("data").get(0)).get("last_name")) );
+        assertEquals(  exp.getJSONArray("data").getJSONObject(0).get("avatar"),( ( (Map)act.getList("data").get(0)).get("avatar")) );
 
     }
 }
