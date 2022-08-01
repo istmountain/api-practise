daf71e48f53c997f0d3e363b53d6afd27f5372c7
EceNarin <la.crymosa321@gmail.com>
2022-08-01 17:44:21 +0300

    add

--- src/test/java/reqresIn/C05_GetSingleResource.java
+++ src/test/java/reqresIn/C05_GetSingleResource.java
@@ -16,14 +16,14 @@ import static io.restassured.RestAssured.given;
 public class C05_GetSingleResource extends BaseReqresIn {
     @Test
     public void http() throws IOException {
-        URL url = new URL("https://reqres.in/api/unknown");
+        URL url = new URL("https://reqres.in/api/unknown/2");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
     }
     @Test
     public void req() {
-        RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://reqres.in/api/unknown").build();
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://reqres.in/api/unknown/2").build();
         Response response=given()
                 .spec(req)
                 .when()
@@ -32,23 +32,35 @@ public class C05_GetSingleResource extends BaseReqresIn {
         response
                 .then()
                 .assertThat()
-                .statusCode(200);
+                .statusCode(200)
+                .body("data.id", Matchers.equalTo(2)
+                        ,"data.name", Matchers.equalTo("fuchsia rose")
+                        ,"data.year", Matchers.equalTo(2001)
+                        ,"data.color", Matchers.equalTo("#C74375")
+                        ,"data.pantone_value", Matchers.equalTo("17-2031")
+                        ,"support.url", Matchers.equalTo("https://reqres.in/#support-heading")
+                        ,"support.text", Matchers.equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
     }
     @Test
-    public void res() { //https://reqres.in/api/unknown
-        specReqres.pathParams("pp1","api","pp2","unknown");
+    public void res() { //https://reqres.in/api/unknown/2
+        specReqres.pathParams("pp1","api","pp2","unknown","pp3",2);
         Response response=given()
                 .spec(specReqres)
                 .when()
-                .get("/{pp1}/{pp2}");
+                .get("/{pp1}/{pp2}/{pp3}");
         response.prettyPrint();
-        response.prettyPrint();
-        int size= response.jsonPath().getList("data").size();
+
         response
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("data", Matchers.hasSize(size));
+                .body("data.id", Matchers.equalTo(2)
+                ,"data.name", Matchers.equalTo("fuchsia rose")
+                ,"data.year", Matchers.equalTo(2001)
+                ,"data.color", Matchers.equalTo("#C74375")
+                ,"data.pantone_value", Matchers.equalTo("17-2031")
+                ,"support.url", Matchers.equalTo("https://reqres.in/#support-heading")
+                ,"support.text", Matchers.equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
     }
 
 
