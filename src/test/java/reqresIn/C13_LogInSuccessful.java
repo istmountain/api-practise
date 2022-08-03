9d60c9aafafcc0fca167c4fefa08acaaedf8a044
EceNarin <la.crymosa321@gmail.com>
2022-08-03 15:42:14 +0300

    add

--- src/test/java/reqresIn/C13_LogInSuccessful.java
+++ src/test/java/reqresIn/C13_LogInSuccessful.java
@@ -7,6 +7,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,58 +17,64 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.replaceFiltersWith;
 import static org.junit.Assert.assertEquals;
 
 public class C13_LogInSuccessful extends BaseReqresIn {
     @Test
     public void http() throws IOException {
-        URL url = new URL("https://reqres.in/api/register");
+        URL url = new URL("https://reqres.in/api/login");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
-        http.setRequestProperty("Content-Type", "application/json");
-
-        String data = "{\n    \"email\": \"eve.holt@reqres.in\",\n    \"password\": \"pistol\"\n}";
-
-        byte[] out = data.getBytes(StandardCharsets.UTF_8);
-
-        OutputStream stream = http.getOutputStream();
-        stream.write(out);
+        http.setRequestProperty("Authorization", "Basic ZXZlLmhvbHRAcmVxcmVzLmluOmNpdHlzbGlja2E=");
 
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
     }
     @Test
     public void req() {
-
+        /*
+        {
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+}
+         */
         JSONObject body=new JSONObject();
-        body.put("email", "sydney@fife");
-        RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://reqres.in/api/register").build();
-        Response response=given()
-                .spec(req)
-                .contentType(ContentType.JSON)
-                .body(body.toString())
-                .when()
-                .post();
-        response.prettyPrint();
-        //asssert
-        response
-                .then()
-                .assertThat()
-                .statusCode(400);
-        //save response
+        body.put("email", "eve.holt@reqres.in");
+        body.put("password", "cityslicka");
+
+       RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://reqres.in/api/login")
+              // .addHeader("Authorization", "Basic ZXZlLmhvbHRAcmVxcmVzLmluOmNpdHlzbGlja2E=")
+        .build();
+       Response response=given()
+               .spec(req)
+               .contentType(ContentType.JSON)
+               .body(body.toString())
+               .when()
+               .post();
+       response.prettyPrint();
+       //expbody
+        /*
+        {
+    "token": "QpwL5tke4Pnpja7X4"
+}
+         */
         JSONObject exp=new JSONObject();
-        exp.put("error", "Missing password");
+        exp.put("token", "QpwL5tke4Pnpja7X4");
+        //save response
         JsonPath act=response.jsonPath();
-        assertEquals(exp.get("error"),act.get("error"));
+        response.then()
+                .assertThat()
+                .statusCode(200);
+        assertEquals(exp.get("token"),act.get("token"));
 
     }
     @Test
     public void res() {
-
-        specReqres.pathParams("pp1","api","pp2","register");
+ //"https://reqres.in/api/login"
+        specReqres.pathParams("pp1","api","pp2","login");
         JSONObject body=new JSONObject();
-        body.put("email", "sydney@fife");
+        body.put("email", "eve.holt@reqres.in");
+        body.put("password", "cityslicka");
         Response response=given()
                 .spec(specReqres)
                 .contentType(ContentType.JSON)
@@ -76,14 +83,13 @@ public class C13_LogInSuccessful extends BaseReqresIn {
                 .post("/{pp1}/{pp2}");
         response.prettyPrint();
         //asssert
-        response
-                .then()
-                .assertThat()
-                .statusCode(400);
-        //save response
         JSONObject exp=new JSONObject();
-        exp.put("error", "Missing password");
+        exp.put("token", "QpwL5tke4Pnpja7X4");
+        //save response
         JsonPath act=response.jsonPath();
-        assertEquals(exp.get("error"),act.get("error"));
+        response.then()
+                .assertThat()
+                .statusCode(200);
+        assertEquals(exp.get("token"),act.get("token"));
     }
 }
