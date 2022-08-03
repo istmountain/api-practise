3460e17b77deace347f74802a03e38c52aad665f
EceNarin <la.crymosa321@gmail.com>
2022-08-03 16:17:50 +0300

    add

--- src/test/java/reqresIn/C15_DelayedResponse.java
+++ src/test/java/reqresIn/C15_DelayedResponse.java
@@ -13,6 +13,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
@@ -66,7 +68,13 @@ public class C15_DelayedResponse extends BaseReqresIn {
         //save response
         JsonPath act=response.jsonPath();
         //assert
-        assertEquals();
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("id"),( ( (Map)act.getList("data").get(0)).get("id")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("email"),( ( (Map)act.getList("data").get(0)).get("email")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("first_name"),( ( (Map)act.getList("data").get(0)).get("first_name")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("last_name"),( ( (Map)act.getList("data").get(0)).get("last_name")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("avatar"),( ( (Map)act.getList("data").get(0)).get("avatar")));
+        assertEquals(exp.getJSONObject("support").get("url"),act.get("support.url"));
+        assertEquals(exp.getJSONObject("support").get("url"),act.get("support.url"));
 
     }
     @Test
@@ -87,5 +95,36 @@ public class C15_DelayedResponse extends BaseReqresIn {
             "avatar": "https://reqres.in/img/faces/5-image.jpg"
         },
          */
+        //https://reqres.in/api/users?delay=3
+        specReqres.pathParams("pp1","api","pp2","users").queryParam("delay",3);
+        Response response= given()
+                .spec(specReqres)
+                .when()
+                .get("/{pp1}/{pp2}");
+        JSONObject exp=new JSONObject();
+        JSONArray data=new JSONArray();
+        JSONObject users=new JSONObject();
+        JSONObject support=new JSONObject();
+        users.put("id", 1);
+        users.put("email", "george.bluth@reqres.in");
+        users.put("first_name", "George");
+        users.put("last_name", "Bluth");
+        users.put("avatar", "https://reqres.in/img/faces/1-image.jpg");
+        support.put("url", "https://reqres.in/#support-heading");
+        support.put("text", "To keep ReqRes free, contributions towards server costs are appreciated!");
+        data.put(0,users);
+        exp.put("data",data);
+        exp.put("support",support);
+        //save response
+        JsonPath act=response.jsonPath();
+        //assert
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("id"),( ( (Map)act.getList("data").get(0)).get("id")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("email"),( ( (Map)act.getList("data").get(0)).get("email")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("first_name"),( ( (Map)act.getList("data").get(0)).get("first_name")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("last_name"),( ( (Map)act.getList("data").get(0)).get("last_name")));
+        assertEquals(exp.getJSONArray("data").getJSONObject(0).get("avatar"),( ( (Map)act.getList("data").get(0)).get("avatar")));
+        assertEquals(exp.getJSONObject("support").get("url"),act.get("support.url"));
+        assertEquals(exp.getJSONObject("support").get("url"),act.get("support.url"));
+
     }
 }
