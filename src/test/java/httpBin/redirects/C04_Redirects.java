c4e9b835843cc8c874e124ce01ba2bd5a5c5f47c
EceNarin <la.crymosa321@gmail.com>
2022-08-06 15:41:23 +0300

    add

--- src/test/java/httpBin/redirects/C04_Redirects.java
+++ src/test/java/httpBin/redirects/C04_Redirects.java
@@ -1,11 +1,17 @@
 package httpBin.redirects;
 
 import baseUrls.BaseHttpBin;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static io.restassured.RestAssured.given;
 
 public class C04_Redirects extends BaseHttpBin {
     @Test
@@ -19,8 +25,25 @@ public class C04_Redirects extends BaseHttpBin {
     }
     @Test
     public void req() {
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/redirect-to").build();
+        Response response=given()
+                .spec(req)
+                .when()
+                .get();
+        response.then()
+                .assertThat()
+                .statusCode(500);
     }
     @Test
     public void res() {
+        specHttpbin.pathParam("pp1","redirect-to");
+        Response response=given()
+                .spec(specHttpbin)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/{pp1}");
+        response.then()
+                .assertThat()
+                .statusCode(500);
     }
 }
