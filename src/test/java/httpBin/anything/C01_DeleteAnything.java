4efe46efc6b273be5dd8092d6043eb0b5b6067a6
EceNarin <la.crymosa321@gmail.com>
2022-08-09 23:59:33 +0300

    add

--- src/test/java/httpBin/anything/C01_DeleteAnything.java
+++ src/test/java/httpBin/anything/C01_DeleteAnything.java
@@ -2,8 +2,10 @@ package httpBin.anything;
 
 import baseUrls.BaseHttpBin;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,6 +13,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C01_DeleteAnything extends BaseHttpBin {
     /*
@@ -65,10 +68,63 @@ Response headers
 
     @Test
     public void req() {
-        RequestSpecification req=new RequestSpecBuilder().setBaseUri().setAccept().build();
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("http://httpbin.org/anything")
+                .setAccept("application/json").build();
         Response response=given()
                 .spec(req)
                 .when().get();
+        response.prettyPrint();
+     response.then()
+             .assertThat()
+             .statusCode(200);
+     /*
+     {
+    "args": {
+
+    },
+    "data": "",
+    "files": {
+
+    },
+    "form": {
+
+    },
+    "headers": {
+        "Accept": "application/json",
+        "Accept-Encoding": "gz1p,deflate",
+        "Host": "httpbin.org",
+        "User-Agent": "Apache-HttpClient/4.5.3 (Java/18.0.1.1)",
+        "X-Amzn-Trace-Id": "Root=1-62f2ca40-7958b8b61df8cad173190dba"
+    },
+    "json": null,
+    "method": "GET",
+    "origin": "176.42.164.88",
+    "url": "http://httpbin.org/anything"
+}
+      */
+        //expected body
+        JSONObject headers=new JSONObject();
+        JSONObject expected=new JSONObject();
+        headers.put("Accept", "text/html");
+        headers.put("Accept-Encoding", "gz1p,deflate");
+        headers.put("Host", "httpbin.org");
+        headers.put("User-Agent", "Apache-HttpClient/4.5.3 (Java/18.0.1.1)");
+        expected.put("origin","176.42.164.88");
+        expected.put("url", "http://httpbin.org/get");
+        expected.put("headers",headers);
+        expected.put()
+        // save response
+        JsonPath actual=response.jsonPath();
+        //assertions
+        assertEquals(expected.getJSONObject("headers").get("Accept"),actual.get("headers.Accept"));
+        assertEquals(expected.getJSONObject("headers").get("Accept-Encoding"),actual.get("headers.Accept-Encoding"));
+        assertEquals(expected.getJSONObject("headers").get("Host"),actual.get("headers.Host"));
+        assertEquals(expected.getJSONObject("headers").get("User-Agent"),actual.get("headers.User-Agent"));
+        assertEquals(expected.get("origin"),actual.get("origin"));
+        assertEquals(expected.get("url"),actual.get("url"));
+        assertEquals(expected.get(),actual.get());
+
+
     }
 
     @Test
