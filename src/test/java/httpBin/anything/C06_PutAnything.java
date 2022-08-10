f22b29381d1392bc6d2873f9ec757e09d79f507e
EceNarin <la.crymosa321@gmail.com>
2022-08-10 17:30:52 +0300

    add

---
+++ src/test/java/httpBin/anything/C06_PutAnything.java
@@ -0,0 +1,42 @@
+package httpBin.anything;
+
+import baseUrls.BaseHttpBin;
+
+public class C06_PutAnything extends BaseHttpBin {
+    /*
+
+Code	Details
+200
+Response body
+Download
+{
+  "args": {},
+  "data": "",
+  "files": {},
+  "form": {},
+  "headers": {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+    "Content-Length": "0",
+    "Host": "httpbin.org",
+    "Origin": "http://httpbin.org",
+    "Referer": "http://httpbin.org/",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.47",
+    "X-Amzn-Trace-Id": "Root=1-62f3c0de-5e4ed3e0399269f10014a95b"
+  },
+  "json": null,
+  "method": "PUT",
+  "origin": "176.42.172.132",
+  "url": "http://httpbin.org/anything"
+}
+Response headers
+ access-control-allow-credentials: true
+ access-control-allow-origin: http://httpbin.org
+ connection: keep-alive
+ content-length: 686
+ content-type: application/json
+ date: Wed, 10 Aug 2022 14:29:50 GMT
+ server: gunicorn/19.9.0
+     */
+}
