7efc9e8d814bceb8a33e90d56275be804c9b2310
EceNarin <la.crymosa321@gmail.com>
2022-08-15 18:46:06 +0300

    add

---
+++ src/test/java/trelloRestApi/C01_GetAPI.java
@@ -0,0 +1,35 @@
+package trelloRestApi;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class C01_GetAPI {
+    /*
+    key=6e0405633cf470268d0a6579d0082fa8
+token=27726bdc8dee5827e17fdd1b4b3148acd6eeec8e8523b5e2b43b84d09e7a0dc6
+key1 = 8fc757f0282852be46596d146f4b3b1b
+token1 = 30734891211be87438de1e97a03c0185e29b0b8c40be8b12937123da12c85c53
+zoken=27726bdc8dee5827e17fdd1b4b3148acd6eeec8e8523b5e2b43b84d09e7a0dc6
+"id": "62d7e33f30d34c3a25a2fc28",
+https://api.trello.com/1/members/ecenarin1
+     */
+
+    @Test
+    public void http() throws IOException {
+        URL url = new URL("https://api.trello.com/1/members/ecenarin1");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestProperty("Authorization", "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y");
+
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        http.disconnect();
+    }
+    @Test
+    public void req() {
+    }
+    @Test
+    public void res() {
+    }
+}
