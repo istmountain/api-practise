c5fcfb9790ed7e1037d47a5d1770da53175b3dbf
EceNarin <la.crymosa321@gmail.com>
2022-08-18 20:00:27 +0300

    add

---
+++ src/test/java/restApi/C04_PostCreateUsers.java
@@ -0,0 +1,24 @@
+package restApi;
+
+import baseUrls.BaseUrlGorest;
+import org.junit.Test;
+
+public class C04_PostCreateUsers extends BaseUrlGorest {
+    /*
+    Create user
+curl -i -H "Accept:application/json" -H "Content-Type:application/json" -H "Authorization:
+ Bearer ACCESS-TOKEN" -XPOST "https://gorest.co.in/public/v2/users" -d '
+ {"name":"Tenali Ramakrishna", "gender":"male",
+ "email":"tenali.ramakrishna@15ce.com", "status":"active"}
+     */
+
+    @Test
+    public void http() {
+    }
+    @Test
+    public void req() {
+    }
+    @Test
+    public void res() {
+    }
+}
