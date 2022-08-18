b030ac632e1fdbd49b0daa67c14624cc208912bd
EceNarin <la.crymosa321@gmail.com>
2022-08-18 20:11:50 +0300

    add

--- src/test/java/restApi/C04_PostCreateUsers.java
+++ src/test/java/restApi/C04_PostCreateUsers.java
@@ -1,6 +1,12 @@
 package restApi;
 
 import baseUrls.BaseUrlGorest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,6 +14,9 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C04_PostCreateUsers extends BaseUrlGorest {
     /*
@@ -39,6 +48,46 @@ curl -i -H "Accept:application/json" -H "Content-Type:application/json" -H "Auth
     }
     @Test
     public void req() {
+    /*
+    Create user
+curl -i -H "Accept:application/json" -H "Content-Type:application/json" -H "Authorization:
+ Bearer ACCESS-TOKEN" -XPOST "https://gorest.co.in/public/v2/users" -d '
+ {"name":"Tenali Ramakrishna", "gender":"male",
+ "email":"tenali.ramakrishna@15ce.com", "status":"active"}
+     */
+        JSONObject body=new JSONObject();
+        body.put("name","Tenali Ramakrishna");
+        body.put("gender","male");
+        body.put("email","tenali.ramakr6768ishna@15ce.com");
+        body.put("status","active");
+        RequestSpecification req=new RequestSpecBuilder()
+                .setBaseUri("https://gorest.co.in/public/v2/users")
+                .setContentType("application/json")
+                .setAccept("application/json")
+                .addHeader("Authorization", "Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79")
+                .build();
+        Response response=given()
+                .spec(req)
+                .body(body.toString())
+                .when()
+                .post();
+        response.prettyPrint();
+        //exp body
+        JSONObject exp=new JSONObject();
+        exp.put("name","Tenali Ramakrishna");
+        exp.put("gender","male");
+        exp.put("email","tenali.ramakr6768ishna@15ce.com");
+        exp.put("status","active");
+        //save response
+        JsonPath act=response.jsonPath();
+        response.then()
+                .assertThat()
+                .statusCode(200);
+        assertEquals(exp.get("name"),act.get("name"));
+        assertEquals(exp.get("gender"),act.get("gender"));
+        assertEquals(exp.get("email"),act.get("email"));
+        assertEquals(exp.get("status"),act.get("status"));
+
     }
     @Test
     public void res() {
