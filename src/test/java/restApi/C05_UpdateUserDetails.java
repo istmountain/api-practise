4549074340a7b28e72e44cf5afe8b453172d082c
EceNarin <la.crymosa321@gmail.com>
2022-08-20 18:03:34 +0300

    add

--- src/test/java/restApi/C05_UpdateUserDetails.java
+++ src/test/java/restApi/C05_UpdateUserDetails.java
@@ -1,5 +1,10 @@
 package restApi;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,6 +13,9 @@ import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C05_UpdateUserDetails {
     /*
@@ -49,6 +57,47 @@ public class C05_UpdateUserDetails {
 
     @Test
     public void req() {
+         /*
+    Update user
+  curl -i -H "Accept:application/json" -H "Content-Type:application/json"
+ -H "Authorization: Bearer ACCESS-TOKEN" -XPATCH "https://gorest.co.in/public/v2/users/24"
+ -d '{"name":"Allasani Peddana", "email":"allasani.peddana@15ce.com", "status":"active"}'
+ "Authorization", "Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79"
+        {
+    "email": "allaasani.peddana@15ce.com",
+    "name": "Allasani Peddana",
+    "status": "active",
+    "id": 24,
+    "gender": "female"
+}
+         */
+        JSONObject body=new JSONObject();
+        body.put("email", "allaasani.peddsdana@15ce.com");
+        body.put("name", "Allasani Peddana");
+        body.put("status", "active");
+        body.put("gender", "female");
+        RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://gorest.co.in/public/v2/users/24")
+                .addHeader("Authorization", "Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79")
+                .setContentType("application/json")
+                .setAccept("application/json")
+                .build();
+        Response response=given()
+                .spec(req)
+                .body(body.toString())
+                .when()
+                .patch();
+        response.prettyPrint();
+        JSONObject exp=new JSONObject();
+        exp.put("email", "allaasani.peddsdana@15ce.com");
+        exp.put("name", "Allasani Peddana");
+        exp.put("status", "active");
+        exp.put("gender", "female");
+        exp.put("id", 24);
+        response
+                .then()
+                .assertThat()
+                .statusCode(200);
+        assertEquals();
     }
 
     @Test
