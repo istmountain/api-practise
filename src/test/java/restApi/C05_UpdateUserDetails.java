d20a53039afcd26024b04f29d14de5d9bf34670a
EceNarin <la.crymosa321@gmail.com>
2022-08-20 17:47:24 +0300

    add

---
+++ src/test/java/restApi/C05_UpdateUserDetails.java
@@ -0,0 +1,24 @@
+package restApi;
+
+import org.junit.Test;
+
+public class C05_UpdateUserDetails {
+    /*
+    Update user
+  curl -i -H "Accept:application/json" -H "Content-Type:application/json"
+ -H "Authorization: Bearer ACCESS-TOKEN" -XPATCH "https://gorest.co.in/public/v2/users/24"
+ -d '{"name":"Allasani Peddana", "email":"allasani.peddana@15ce.com", "status":"active"}'
+     */
+
+    @Test
+    public void http() {
+    }
+
+    @Test
+    public void req() {
+    }
+
+    @Test
+    public void res() {
+    }
+}
