ca445f24ff82ea2bb2d4e85da399ee4b09eea7a2
EceNarin <la.crymosa321@gmail.com>
2022-08-21 18:55:11 +0300

    add

---
+++ src/test/java/restApi/C06_DeleteRequest.java
@@ -0,0 +1,24 @@
+package restApi;
+
+import baseUrls.BaseRestApi;
+import org.junit.Test;
+
+public class C06_DeleteRequest extends BaseRestApi {
+    /*
+    Delete user
+curl -i -H "Accept:application/json"
+-H "Content-Type:application/json"
+-H "Authorization: Bearer ACCESS-TOKEN"
+-XDELETE "https://gorest.co.in/public/v2/users/24"
+     */
+
+    @Test
+    public void http() {
+    }
+    @Test
+    public void req() {
+    }
+    @Test
+    public void res() {
+    }
+}
