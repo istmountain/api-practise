fea84323e99311358bf7793249cc3f06b93e156f
EceNarin <la.crymosa321@gmail.com>
2022-08-22 12:04:00 +0300

    add

--- src/test/java/restApi/curlForGraphQL/C02_GetUserGraphQL.java
+++ src/test/java/restApi/curlForGraphQL/C02_GetUserGraphQL.java
@@ -1,6 +1,9 @@
 package restApi.curlForGraphQL;
 
 import baseUrls.BaseUrlGorest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,6 +12,8 @@ import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.given;
 
 public class C02_GetUserGraphQL extends BaseUrlGorest {
     /*
@@ -53,6 +58,54 @@ public class C02_GetUserGraphQL extends BaseUrlGorest {
 
     @Test
     public void req() {
+            /*
+    curl -i -H "Accept:application/json"
+    -H "Content-Type:application/json"
+    -H "Authorization: Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79"
+    -XPOST "https://gorest.co.in/public/v2/graphql"
+    -d '{"query":"query{user(id: 32) { id name email gender status }}"}'
+    {
+    "data": {
+        "user": {
+            "id": 32,
+            "name": "Chandrabhan Nair Sr.",
+            "email": "sr_nair_chandrabhan@kuhlman.net",
+            "gender": "male",
+            "status": "active"
+        }
+    }
+}
+     */
+        String data = "{\"query\":\"query{user(id: 32) { id name email gender status }}\"}";
+        RequestSpecification req=new RequestSpecBuilder()
+                .setBaseUri("https://gorest.co.in/public/v2/graphql")
+                .setContentType("application/json")
+                .setAccept("application/json")
+                .addHeader("Authorization", "Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79")
+                .build();
+        Response response=
+                given()
+                        .spec(req)
+                        .body(data)
+                        .when()
+                        .post();
+        response.prettyPrint();
+        //expBody
+        /*
+        {
+    "data": {
+        "user": {
+            "id": 32,
+            "name": "Dhruv Panicker",
+            "email": "panicker_dhruv@feil.org",
+            "gender": "male",
+            "status": "inactive"
+        }
+    }
+}
+
+         */
+
     }
 
     @Test
