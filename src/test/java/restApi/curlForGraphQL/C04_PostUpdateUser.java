fa3bbe369ecbf82a597a240b229857f9f0cba489
EceNarin <la.crymosa321@gmail.com>
2022-08-27 20:01:50 +0300

    add

---
+++ src/test/java/restApi/curlForGraphQL/C04_PostUpdateUser.java
@@ -0,0 +1,26 @@
+package restApi.curlForGraphQL;
+
+import org.junit.Test;
+
+public class C04_PostUpdateUser {
+    @Test
+    public void HTTP() {
+        /*
+        Update user
+curl -i -H "Accept:application/json" -H "Content-Type:application/json"
+-H "Authorization: Bearer ACCESS-TOKEN" -XPATCH "https://gorest.co.in/public/v2/users/9"
+-d '{"name":"Allasani Peddana", "email":"allasani.peddana@15ce.com", "status":"active"}'
+         */
+
+    }
+
+    @Test
+    public void req() {
+             /*
+        Update user
+curl -i -H "Accept:application/json" -H "Content-Type:application/json"
+-H "Authorization: Bearer ACCESS-TOKEN" -XPATCH "https://gorest.co.in/public/v2/users/9"
+-d '{"name":"Allasani Peddana", "email":"allasani.peddana@15ce.com", "status":"active"}'
+         */
+    }
+}
