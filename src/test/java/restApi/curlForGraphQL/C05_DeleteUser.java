026e29e5a27ffc5620d2ee678b5dc91684c8e726
EceNarin <la.crymosa321@gmail.com>
2022-08-28 14:14:03 +0300

    add

--- src/test/java/restApi/curlForGraphQL/C05_DeleteUser.java
+++ src/test/java/restApi/curlForGraphQL/C05_DeleteUser.java
@@ -1,2 +1,12 @@
-package restApi.curlForGraphQL;public class C05_DeleteUser {
+package restApi.curlForGraphQL;
+
+import baseUrls.BaseUrlGorest;
+
+public class C05_DeleteUser extends BaseUrlGorest {
+    /*
+    curl -i -H "Accept:application/json"
+    -H "Content-Type:application/json"
+    -H "Authorization: Bearer ACCESS-TOKEN" -XPOST "https://gorest.co.in/public/v2/graphql" ,
+    -d '{"query":"mutation{deleteUser(input: {id: 9}){user {id name email gender status}}}"}'
+     */
 }
