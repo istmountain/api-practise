8c07fa7a7afb576cb169152303ae7d645a2be37e
EceNarin <la.crymosa321@gmail.com>
2022-08-30 20:31:24 +0300

    add

--- src/test/java/restApi/curlForGraphQL/C05_DeleteUser.java
+++ src/test/java/restApi/curlForGraphQL/C05_DeleteUser.java
@@ -1,6 +1,9 @@
 package restApi.curlForGraphQL;
 
 import baseUrls.BaseUrlGorest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,6 +12,8 @@ import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
+import static io.restassured.RestAssured.given;
 
 public class C05_DeleteUser extends BaseUrlGorest {
     /*
@@ -60,5 +65,27 @@ public class C05_DeleteUser extends BaseUrlGorest {
 
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
         http.disconnect();
+    }
+
+    @Test
+    public void req() {
+        String data = "{\"query\":\"mutation{deleteUser(input: {id: 9}){user {id name email gender status}}}\"}";
+        RequestSpecification req=new RequestSpecBuilder()
+                .setBaseUri("https://gorest.co.in/public/v2/graphql")
+                .setContentType("application/json")
+                .setAccept("application/json")
+                .addHeader("Authorization", "Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79")
+                .build();
+        Response response=given()
+                .spec(req)
+                .body(data)
+                .when()
+                .post();
+        response.prettyPrint();
+        response
+                .then()
+                .assertThat()
+                .statusCode(200);
+
     }
 }
