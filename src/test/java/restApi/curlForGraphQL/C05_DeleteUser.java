c2aa8e70b0aafb7976ffe760376433ede6eb603b
EceNarin <la.crymosa321@gmail.com>
2022-08-30 20:27:50 +0300

    add

--- src/test/java/restApi/curlForGraphQL/C05_DeleteUser.java
+++ src/test/java/restApi/curlForGraphQL/C05_DeleteUser.java
@@ -1,6 +1,14 @@
 package restApi.curlForGraphQL;
 
 import baseUrls.BaseUrlGorest;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class C05_DeleteUser extends BaseUrlGorest {
     /*
@@ -9,4 +17,48 @@ public class C05_DeleteUser extends BaseUrlGorest {
     -H "Authorization: Bearer ACCESS-TOKEN" -XPOST "https://gorest.co.in/public/v2/graphql" ,
     -d '{"query":"mutation{deleteUser(input: {id: 9}){user {id name email gender status}}}"}'
      */
+    /*
+    Response
+    {
+    "data": {
+        "deleteUser": null
+    },
+    "errors": [{
+        "message": "Resource not found!",
+        "locations": [{
+            "line": 1,
+            "column": 10
+        }],
+        "path": ["deleteUser"],
+        "extensions": {
+            "code": "NOT_FOUND",
+            "result": [{
+                "fieldName": "id",
+                "messages": ["Couldn't find record with id=9"]
+            }]
+        }
+    }]
+}
+     */
+
+    @Test
+    public void http() throws IOException {
+        URL url = new URL("https://gorest.co.in/public/v2/graphql");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("Content-Type", "application/json");
+        http.setRequestProperty("Accept", "application/json");
+        http.setRequestProperty("Authorization", "Bearer acc5b803a5df9bc89143ebc8a78d79e0c7dd9ea02e0f98717e2cf22dd60fac79");
+
+        String data = "{\"query\":\"mutation{deleteUser(input: {id: 9}){user {id name email gender status}}}\"}";
+
+        byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+        OutputStream stream = http.getOutputStream();
+        stream.write(out);
+
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        http.disconnect();
+    }
 }
