3ad0d918d10fffbd8e9f99303df2592aecf142e1
Zafer <zafertapikara1@gmail.com>
2022-12-15 21:35:26 +0100

    v1

--- src/test/java/baseUrls/BaseHttpBin.java
+++ src/test/java/baseUrls/BaseHttpBin.java
@@ -6,9 +6,10 @@ import org.junit.Before;
 
 public class BaseHttpBin {
     ///
-    //
+    /////
     protected RequestSpecification specHttpbin;
     @Before
+    //
     //
     public void setup(){
         specHttpbin=new RequestSpecBuilder().setBaseUri("http://httpbin.org").build();
